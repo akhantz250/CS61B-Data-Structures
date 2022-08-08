24647addfcd75fdc773768a83990e55da7d253d5
Aung Khant Zaw <akhantz250@gmail.com>
2022-08-08 20:11:16 +0800

    Created a MaxArrayDeque class

--- proj1/deque/MaxArrayDeque.java
+++ proj1/deque/MaxArrayDeque.java
@@ -10,15 +10,20 @@ public class MaxArrayDeque<Type> extends ArrayDeque<Type>{
     // uses the default comparator given in the constructor
     public Type max() {
         // write out the logic that iterates through the array using the default comparator's compare method
-        return null;
+        return max(defaultComparator);
     }
     // uses the comparator given in the argument
     public Type max(Comparator<Type> c) {
         // write out the logic that iterates through the array using the superclass methods
         // and use the comparator's compare method
-        return null;
+        Type maxElement = this.get(0);
+        for (int i = 0; i < this.size(); i++) {
+            Type currentElement = this.get(i);
+            if (c.compare(currentElement, maxElement) > 0) {
+                maxElement = currentElement;
+            }
+        }
+        return maxElement;
     }
 }
-
-// make additional comparators classes for different types
 
